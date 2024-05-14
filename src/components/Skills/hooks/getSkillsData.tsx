5d8f59e741a79c9a0da87445d3ff488b3702eaa4
Alexandre Beck <alexandrebeck2003@gmail.com>
2024-05-14 14:12:39 -0300

    add pattern to skill icons

--- src/components/Skills/hooks/getSkillsData.tsx
+++ src/components/Skills/hooks/getSkillsData.tsx
@@ -1,101 +1,116 @@
 import { Skill } from "@/types/skill-card";
 import { useTranslation } from "react-i18next";
-import { FaDocker, FaJs, FaNodeJs, FaReact } from "react-icons/fa";
-import { SiBootstrap, SiExpress, SiFigma, SiFlutter, SiJest, SiMongodb, SiNestjs, SiPostgresql, SiStyledcomponents, SiTailwindcss, SiTypescript } from "react-icons/si";
+import {
+    SiBootstrap,
+    SiDocker,
+    SiExpress,
+    SiFigma,
+    SiFlutter,
+    SiJavascript,
+    SiJest,
+    SiMongodb,
+    SiNestjs,
+    SiNodedotjs,
+    SiPostgresql,
+    SiReact,
+    SiStyledcomponents,
+    SiTailwindcss,
+    SiTypescript,
+} from "react-icons/si";
 
 export function useGetSkillsData() {
     const { t } = useTranslation();
 
     const skillsData: Skill[] = [
         {
-            Icon: FaReact,
+            Icon: SiReact,
             title: "React.js",
             description: t("skillsData.react"),
-            years: 2.2
+            years: 2.2,
         },
         {
             Icon: SiTailwindcss,
             title: "Tailwind CSS",
             description: t("skillsData.tailwind"),
-            years: 0.6
+            years: 0.6,
         },
         {
             Icon: SiStyledcomponents,
             title: "Styled Components",
             description: t("skillsData.styled"),
-            years: 1.2
+            years: 1.2,
         },
         {
             Icon: SiBootstrap,
             title: "Bootstrap",
             description: t("skillsData.bootstrap"),
-            years: 1.6
+            years: 1.6,
         },
         {
             Icon: SiJest,
             title: "Jest",
             description: t("skillsData.jest"),
-            years: 1.2
+            years: 1.2,
         },
         {
-            Icon: FaDocker,
+            Icon: SiDocker,
             title: "Docker",
             description: t("skillsData.docker"),
-            years: 0.5
+            years: 0.5,
         },
         {
-            Icon: FaJs,
+            Icon: SiJavascript,
             title: "JavaScript",
             description: t("skillsData.javascript"),
-            years: 2.8
+            years: 2.8,
         },
         {
             Icon: SiTypescript,
             title: "TypeScript",
             description: t("skillsData.typescript"),
-            years: 2.4
+            years: 2.4,
         },
         {
             Icon: SiPostgresql,
             title: "PostgreSQL",
             description: t("skillsData.postgree"),
-            years: 1.4
+            years: 1.4,
         },
         {
-            Icon: FaNodeJs,
+            Icon: SiNodedotjs,
             title: "Node.js",
             description: t("skillsData.node"),
-            years: 0.6
+            years: 0.6,
         },
         {
             Icon: SiExpress,
             title: "Express.js",
             description: t("skillsData.express"),
-            years: 0.4
+            years: 0.4,
         },
         {
             Icon: SiNestjs,
             title: "Nest.js",
             description: t("skillsData.nest"),
-            years: 0.4
+            years: 0.4,
         },
         {
             Icon: SiMongodb,
             title: "MongoDB",
             description: t("skillsData.mongo"),
-            years: 0.6
+            years: 0.6,
         },
         {
             Icon: SiFigma,
             title: "Figma",
             description: t("skillsData.figma"),
-            years: 0.6
+            years: 0.6,
         },
         {
             Icon: SiFlutter,
             title: "Flutter",
             description: t("skillsData.flutter"),
-            years: 0.4
+            years: 0.4,
         },
     ];
 
