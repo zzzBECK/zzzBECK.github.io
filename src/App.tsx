40106824adb5f5905852ea444a3f9beecd627976
Alexandre Beck <alexandrebeck2003@gmail.com>
2024-05-13 22:52:27 -0300

    added layout to fallback

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,19 @@ import { Projects } from "./components/Projects/projects";
 import { Skills } from "./components/Skills/skills";
 import { ThemeProvider } from "./components/theme-provider";
 import { Toaster } from "./components/ui/sonner";
+import { Skeleton } from "./components/ui/skeleton";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense
+        fallback={
+          <div className="flex flex-col items-center justify-center w-svw h-svh px-4 md:px-12 lg:px-24 xl:px-52 py-12 gap-4">
+            <Skeleton className="w-full h-1/4" />
+            <Skeleton className="w-full h-3/4" />
+          </div>
+        }
+      >
         <main className="w-screen min-h-screen flex flex-col">
           <Header />
           <Introduction />
